09da64870f6dd8e25b787629de4da4b9b7685112
camilaalmeida-receitas <pedromartinstests@gmail.com>
2025-08-10 22:40:30 -0300

    Add DB

--- backend/settings.py
+++ backend/settings.py
@@ -176,7 +176,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL'),
-        conn_max_age=600,
+        conn_max_age=60,
         ssl_require=True
     )
 }
